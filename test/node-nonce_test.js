0af7da493d78efc942e948e70b3ec51da2e0812b
Andreas Hultgren <andreas.hultgren@frojd.se>
2013-01-06 22:01:39 +0100

    Added tests for POST and already used nonces

--- test/node-nonce_test.js
+++ test/node-nonce_test.js
@@ -30,7 +30,7 @@ exports['generate and validate valid nonce through express and GET'] = function 
     res = {},
     doneCount = 0;
   
-  test.expect(3);
+  test.expect(4);
 
   nonce.use(function (req, res, nonceItem, next) {
     test.equal(req.user, nonceItem.data.user);
@@ -44,6 +44,46 @@ exports['generate and validate valid nonce through express and GET'] = function 
 
     validate(req, res, function(err){
       test.equal(err instanceof Error, false, 'Validation should not throw an error');
+
+      validate(req, res, function(err){
+        test.equal(err instanceof Error, true, 'Validation 2 should throw an error');
+
+        test.done();
+      });
+    });
+  });
+};
+
+exports['generate and validate valid nonce through express and POST'] = function (test) {
+  var validate = nonce.verify('test'),
+    req = {
+      method: 'POST',
+      user: 'test',
+      body: {}
+    },
+    res = {},
+    doneCount = 0;
+  
+  test.expect(4);
+
+  nonce.use(function (req, res, nonceItem, next) {
+    test.equal(req.user, nonceItem.data.user);
+    next();
+  });
+
+  nonce.generate({ action: 'test', user: 'test' }, function (nonce) {
+    test.equal(typeof nonce, typeof '', 'Type of nonce is not string');
+
+    req.body.nonce = nonce;
+
+    validate(req, res, function(err){
+      test.equal(err instanceof Error, false, 'Validation should not throw an error');
+
+      validate(req, res, function(err){
+        test.equal(err instanceof Error, true, 'Validation 2 should throw an error');
+
+        test.done();
+      });
 
       test.done();
     });
